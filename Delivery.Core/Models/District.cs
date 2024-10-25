7585c43270f02e39ad945e2e38831fe13872756c
Roman <roman.songoku.army@gmail.com>
2024-10-25 12:10:03 +0700

    Сделана общая структура проекта + добавлена модель данных

---
+++ Delivery.Core/Models/District.cs
@@ -0,0 +1,28 @@
+﻿namespace Delivery.Core.Models
+{
+    public class District
+    {
+        private const int MIN_TITLE_LEN = 3;
+        private const int MAX_TITLE_LEN = 100;
+        private District(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+        public int Id { get; }
+        public string Title { get; }
+        public static (District District, string Error) Create(int id, string title)
+        {
+            var error = string.Empty;
+
+            if (string.IsNullOrEmpty(title)) error = "Title can not be empty";
+            if (title.Length < MIN_TITLE_LEN) error = "Title can not be less than 3 symbols";
+            if (title.Length < MAX_TITLE_LEN) error = "Title can not be more than 100 symbols";
+
+            var district = new District(id, title);
+
+            return (district, error);
+        }
+           
+    }
+}
---
+++ Delivery.Core/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿namespace Delivery.Core.Models
+{
+    public class Order
+    {
+        public const double MIN_WEIGHT = 0.01;
+        public const double MAX_WEIGHT = 100.0;
+        private Order(Guid id, double weight, District district, DateTime deliveryTime)
+        {
+            Id = id;
+            Weight = weight;
+            District = district;
+            DeliveryTime = deliveryTime;
+        }
+
+        public Guid Id { get; }
+        public double Weight { get; }
+        public District District { get; }
+        public DateTime DeliveryTime { get; }
+        public static (Order Order, string Error) Create(Guid id, double weight, District district, DateTime deliveryTime)
+        {
+            var error = string.Empty;
+
+            if (weight < MIN_WEIGHT) error = "Weight can not be less than 0.01";
+            if (weight > MAX_WEIGHT) error = "Weight can not be more than 100.0";
+
+            var order = new Order(id, weight, district, deliveryTime);
+
+            return (order, error);
+        }
+    }
+}
